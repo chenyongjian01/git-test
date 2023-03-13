29812cee9177fb0efdb10ca777b51dffdde16929
1576259425@qq.com <cyj15697742130>
2023-03-13 18:42:18 +0800

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName:test
+ * Package:PACKAGE_NAME
+ * Description:
+ * Author:cccccyj
+ *
+ * @Create:2023/3/13 - 18:31
+ * Version:v1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
