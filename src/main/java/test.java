f1523e5d8e2768d96bbc2af485ad62757e485d08
chenyongjian01 <116260146+chenyongjian01@users.noreply.github.com>
2023-03-13 18:52:19 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -11,5 +11,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("bbb");
+        System.out.println("ddd");
     }
 }
